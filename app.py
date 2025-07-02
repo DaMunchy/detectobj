5035d2789581bb5ce1687782f440a5e0affacf66
Munchy <munchynes@gmail.com>
2025-07-02 10:09:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -49,4 +49,7 @@ def detect_image():
     return send_file(buf, mimetype='image/jpeg')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
